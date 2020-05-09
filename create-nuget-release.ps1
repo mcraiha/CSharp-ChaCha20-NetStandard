0c1feab825fed16d564f7c517eec7cabb8cfbf3f
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-05-09 13:45:29 +0300

    Update relase script
    Add git short hash

--- create-nuget-release.ps1
+++ create-nuget-release.ps1
@@ -1,3 +1,4 @@
 $currentDate = Get-Date
-$finalCommand = "dotnet pack" + " " + "--configuration Release" + " " + "--include-source" + " " + "--include-symbols" + " " + "/p:InformationalVersion=""" + $currentDate.ToUniversalTime().ToString("yyyy-MM-dd HH.mm.ss") + """"
+$gitShortHash = ((git rev-parse --short HEAD) | Out-String).Trim()
+$finalCommand = "dotnet pack" + " " + "--configuration Release" + " " + "--include-source" + " " + "--include-symbols" + " " + "/p:InformationalVersion=""" + "Build time: " + $currentDate.ToUniversalTime().ToString("yyyy-MM-dd HH.mm.ss") + " Short hash: " + $gitShortHash + """"
 Write-Host $finalCommand
